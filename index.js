fe639c6e9d3dab6b0fb3a8826693b270c9479129
phips28 <phips28@gmx.at>
2018-03-27 15:56:59 -0700

    added user agent

--- index.js
+++ index.js
@@ -43,7 +43,12 @@ if (process.env.ENVIRONMENT !== "BROWSER") {
             xhr.send();
         };
     } else {
-        var Request = require('request').defaults({ encoding: null });
+        var Request = require('request').defaults({ 
+            encoding: null,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.46 Safari/537.36"
+            },
+        });
     }
 }
 
